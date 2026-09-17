ddd6314befe850663f59964a52c30c98c48dacd3
Abu Rayhan Kobir <aburayhankobir013@gmail.com>
2026-09-17 14:55:08 +0600

    Introduction to typescript

---
+++ dist/introduction.js
@@ -0,0 +1,8 @@
+"use strict";
+/*
+
+1. Typescript is a syntactic superset of javascript which adds static typing.
+
+*/
+const defination = "Typescript is a syntactic superst of javascript which adds static typing.";
+console.log(defination);
---
+++ introduction.ts
@@ -0,0 +1,9 @@
+/* 
+
+1. Typescript is a syntactic superset of javascript which adds static typing.
+
+*/
+
+const defination: string = "Typescript is a syntactic superst of javascript which adds static typing.";
+
+console.log (defination);
