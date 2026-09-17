d4337a8e43bfc045fa7d7c68239615d1e65c6507
Abu Rayhan Kobir <aburayhankobir013@gmail.com>
2026-09-17 16:07:08 +0600

    Type annotation included

--- typeAnnotations.ts
+++ typeAnnotations.ts
@@ -17,3 +17,32 @@ function greet (name: string): string {
 
 const message: string = greet ("Abu Rayhan Kobir");
 console.log (message);
+
+// Type inference
+// Typescript can automatically determaine (infer) the type of a variable based on its initial value.
+
+let username = "Abu Rayhan Kobir"; // Typescript infers "string"
+
+let score = 100; // Typescript infers number
+
+let flags = [true, false, true]; // Typescript infers boolean []
+
+function sum (number1: number, number2: number) {
+  return number1 + number2;
+} // Typescript infers return type will be number 
+
+interface Person {
+  name: string;
+  age: number;
+  isAdmin: boolean;
+}
+
+const person: Person = {
+  name: "Abu Rayhan Kobir",
+  age: 30,
+  isAdmin: true,
+}; // Typescritpt inters the shape of the object
+
+console.log (person.age);
+console.log (person.name);
+console.log (person.isAdmin);
